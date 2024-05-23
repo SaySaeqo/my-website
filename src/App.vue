26d5f9eb306480c8ae73d69805204f5f7c4de20f
saysaeqo <saysaeqo@gmail.com>
2024-05-23 20:36:48 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul ref="subpages">
+    <li v-for="page in pages" :key="page">
+      <a :href="page">{{ page }}</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      pages: [
+        "subpage1",
+        "subpage2"
+      ]
+    };
+  },
+});
+</script>
+
+<style scoped>
+ul {
+  list-style-type: square;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
